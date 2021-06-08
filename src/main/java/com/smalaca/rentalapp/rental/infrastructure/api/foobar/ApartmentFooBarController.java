ca011a2936b35bf8ac8c3617c1eb13088db24538
smalaca <ab13.krakow@gmail.com>
2021-06-08 09:51:12 +0200

    Introduce entry points to our application for apartment rental

---
+++ src/main/java/com/smalaca/rentalapp/rental/infrastructure/api/foobar/ApartmentFooBarController.java
@@ -0,0 +1,9 @@
+package com.smalaca.rentalapp.rental.infrastructure.api.foobar;
+
+public class ApartmentFooBarController {
+    public void searchApartments() {}
+    public void filterApartments() {}
+    public void sortApartments() {}
+    public void displayApartment() {}
+    public void chooseApartment() {}
+}
---
+++ src/main/java/com/smalaca/rentalapp/rental/infrastructure/api/foobar/BookingFooBarController.java
@@ -0,0 +1,7 @@
+package com.smalaca.rentalapp.rental.infrastructure.api.foobar;
+
+public class BookingFooBarController {
+    public void bookApartment() {
+
+    }
+}
